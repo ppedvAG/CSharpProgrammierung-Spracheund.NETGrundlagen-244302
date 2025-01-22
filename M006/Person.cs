ce64d8e85d686571c3edb6d392131c2d4636fb14
ppedvLukasK <lukask@ppedv.de>
2025-01-22 12:29:20 +0100

    M006

---
+++ M006/Person.cs
@@ -0,0 +1,144 @@
+﻿namespace M006;
+
+/// <summary>
+/// - Vorname - string
+/// - Nachname - string
+/// - Größe - double
+/// - Alter - int
+/// </summary>
+public class Person
+{
+	#region Feld
+	/// <summary>
+	/// Feld
+	/// Dieses Feld hält einen Wert (hier Vorname)
+	/// Felder sollten nur indirekt angegriffen werden können
+	/// Durch den private Modifier kann das Feld nicht von außerhalb der Klasse angegriffen werden
+	/// 
+	/// Beispiel: Verhindern, dass der User hier beliebige Texte eintragen kann (z.B. Zahlen)
+	/// </summary>
+	private string vorname;
+
+	/// <summary>
+	/// Die Get-Methode wird verwendet, um den Wert aus einem Feld auszulesen
+	/// </summary>
+	public string GetVorname()
+	{
+		return vorname;
+	}
+
+	/// <summary>
+	/// Die Set-Methode wird verwendet, um neue Werte in das Feld einzutragen
+	/// Hier kann der User daran gehindert werden, ungültige Werte einzutragen
+	/// Die Set-Methode ist immer void und hat immer einen Parameter der mit dem dahinterliegenden Feld übereinstimmt
+	/// </summary>
+	public void SetVorname(string vorname)
+	{
+		//this: Wird verwendet, um gleichnamige Variablen voneinander zu unterscheiden
+		//this.vorname bezieht sich auf das Feld weiter oben
+		//vorname bezieht sich auf den Parameter innerhalb dieser Methode
+		if (vorname.All(char.IsLetter) && vorname.Length >= 2 && vorname.Length <= 15)
+			this.vorname = vorname;
+		else
+            Console.WriteLine($"{vorname} ist kein gültiger Vorname");
+    }
+	#endregion
+
+	#region Property
+	/// <summary>
+	/// Property
+	/// Vereinfachung von dem alten Get-/Setmethoden Schema
+	/// Verwendet eine kompakte Syntax
+	/// </summary>
+	private string nachname;
+
+	/// <summary>
+	/// Full Property
+	/// Das Full Property ist ein Property mit einem privaten Feld + einem public Property mit get und set Accessoren
+	/// </summary>
+	public string Nachname
+	{
+		//get
+		//{
+		//	return nachname;
+		//}
+
+		get => nachname;
+		set
+		{
+			//Für den Parameter der Set-Methode gibt es hier ein Keyword: value
+			//value ist hier der neue Wert, welcher vom User gesetzt wird
+			if (value.All(char.IsLetter) && value.Length >= 2 && value.Length <= 15)
+				nachname = value;
+			else
+				Console.WriteLine($"{value} ist kein gültiger Nachname");
+		}
+	}
+
+	/// <summary>
+	/// Get-Only Property
+	/// Ein Get-Only Property kann nicht gesetzt
+	/// Es ist nur dafür da, einen Wert zu erzeugen (bestehend aus anderen Werten)
+	/// </summary>
+	public string VollerName
+	{
+		get
+		{
+			return vorname + " " + nachname;
+		}
+	}
+
+	public string VollerName2
+	{
+		get => vorname + " " + nachname;
+	}
+
+	public string VollerName3 => vorname + " " + nachname;
+
+	/// <summary>
+	/// Auto-Property
+	/// Property, welches kein Feld dahinter hat
+	/// Gleiche Funktionsweise wie eine Variable
+	/// Verwendungen:
+	/// - private set, kann von außen gelesen aber nicht beschrieben werden
+	/// - Json, XML: Felder werden beim Serialisieren nicht berücksichtigt -> Properties
+	/// - WPF/MAUI: Bindings
+	/// - ...
+	/// </summary>
+	public int Alter { get; set; }
+	#endregion
+
+	#region Konstruktor
+	/// <summary>
+	/// Konstruktor
+	/// "Main Methode" von der Klasse
+	/// Wird bei Erstellung von dem Objekt ausgeführt
+	/// Der Standardkonstruktor existiert immer, wenn kein anderer Konstruktor definiert ist
+	/// </summary>
+	public Person()
+	{
+        Console.WriteLine("Person erstellt");
+    }
+
+	/// <summary>
+	/// Hier können jetzt Standardwerte bei Initialisierung übergeben werden
+	/// </summary>
+	public Person(string vorname, string nachname) : this()
+	{
+		this.vorname = vorname;
+		this.nachname = nachname;
+    }
+
+	/// <summary>
+	/// Verkettung von Konstruktoren
+	/// Mithilfe von this(...) können Konstruktoren miteinander verkettet werden
+	/// D.h. wenn dieser Konstruktor ausgeführt wird, wird auch der Konstruktor in der Kette darüber ausgeführt
+	/// </summary>
+	public Person(string vorname, string nachname, int alter) : this(vorname, nachname)
+	{
+		//this.vorname = vorname;
+		//this.nachname = nachname;
+		this.Alter = alter;
+	}
+	#endregion
+}
---
+++ M006/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace M006;
+
+/// <summary>
+/// Klassen und Objekte
+/// 
+/// Objekte sind Datenstrukturen, welche Daten halten
+/// Jedes Objekt kommt aus einer Klasse
+/// Die Klasse hinter den Objekten gibt den Aufbau der Objekte vor
+/// 
+/// int: Ganze Zahl
+/// double: Gleitkommazahl
+/// string: Text
+/// Person: ?
+/// 
+/// Um komplexe Datentypen darzustellen (u.a. Person) benötigen wir Klassen
+/// Die Klasse ist ein Bauplan für Objekte. In der Klasse werden nur Definitionen angelegt
+/// Generell werden Klassen in zwei Typen unterteilt: Datenklassen und Funktionsklassen
+/// 
+/// Person ist eine Datenklasse mit bestimmten Eigenschaften
+/// - Größe - double
+/// - Alter - int
+/// - Vorname - string
+/// - Nachname - string
+/// - ...
+/// 
+/// WICHTIG: In der Klasse werden keine konkreten Werte definiert
+/// Aus der Klasse können beliebig viele Objekte erzeugt werden
+/// Die konkreten Werte werden in den Objekten definiert
+/// Objekt p1: [180cm, 25J, Max, Mustermann]
+/// Objekt p2: [175cm, 20J, Udo, Mustermann]
+/// </summary>
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		//Objekt erstellen mithilfe des new-Keywords
+		Person p1 = new Person();
+		//Hier soll jetzt die Person konkrete Werte bekommen
+		p1.SetVorname("Max"); //Set Methode aufrufen
+		p1.Nachname = "Mustermann"; //Property kann einfach per = zugewiesen werden
+		p1.Alter = 25;
+
+		//Mit Vorname, Nachname Konstruktor
+		Person p2 = new Person("Max", "Mustermann");
+		p2.Alter = 30;
+
+		Person p3 = new Person("Max", "Mustermann", 40);
+	}
+}
