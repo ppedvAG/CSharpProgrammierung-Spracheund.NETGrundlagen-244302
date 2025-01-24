88e20407aaf2e69826d9fcea64726f810723db6c
ppedvLukasK <lukask@ppedv.de>
2025-01-24 15:07:40 +0100

    M015

---
+++ M015/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace M015;
+
+public partial class MainWindow : Window
+{
+	private int Counter;
+
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		Dropdown.ItemsSource = Enumerable.Range(0, 20);
+		LB.ItemsSource = Enumerable.Range(0, 10);
+	}
+
+	/// <summary>
+	/// Events
+	/// Schnittstelle, welche uns ermöglicht, an internen Code eigenen Code anzuhängen
+	/// 
+	/// Entwickler stellen eine Schnittstelle bereit (z.B. Click beim Button)
+	/// Wir als Benutzer können an diese Schnittstelle eigenen Code anhängen
+	/// Wenn intern das Event gefeuert wird, wird unser Code ausgeführt
+	/// </summary>
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		new MainWindow().Show();
+		return;
+
+		Counter++;
+		//Output.Text = Counter.ToString();
+
+		//Output.Text = Dropdown.SelectedItem.ToString();
+
+		MessageBoxResult res = MessageBox.Show("Möchtest du den eingegebenen Text anzeigen?", "Text anzeigen", MessageBoxButton.YesNo, MessageBoxImage.Question);
+        if (res == MessageBoxResult.Yes)
+		{
+			Output.Text = Input.Text;
+		}
+	}
+}
